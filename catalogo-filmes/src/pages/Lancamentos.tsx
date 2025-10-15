baec74b71204198528b408540ae88da9970fa1e3
Henrique de Oliveira <henrique2801oliveira@gmail.com>
2025-10-15 20:00:15 -0300

    Adding a input text in Lancamentos

--- catalogo-filmes/src/pages/Lancamentos.tsx
+++ catalogo-filmes/src/pages/Lancamentos.tsx
@@ -2,11 +2,11 @@ function Lancamentos() {
     return (
         <>
             <div className='bg-[#525252] px-15 py-10 text-white'>
-                <div className="mt-10 ">
+                <div className="mt-10 flex justify-center">
                     <input
                         type="text"
                         placeholder="Pesquisar..."
-                        className="justify-center px-4 py-2 rounded-2xl bg-white text-black placeholder-gray-400 w-150 "
+                        className="justify-center px-4 py-2 rounded-2xl bg-[#D9D9D9] text-black placeholder-gray-400 w-150 border-red-500 border-2 focus:border-[#0F13FC] focus:outline-none"
                     />
                 </div>
             </div>
